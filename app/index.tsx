c1a8b539cbde954240cf88c8fb6ee8b715eb06aa
Htoomyat99 <htoomyat.20399@gmail.com>
2024-10-28 23:34:06 +0630

    add splash and app icon and prebuild

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Index = () => {
+  return (
+    <SafeAreaView>
+      <Text>Index</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Index;
+
+const styles = StyleSheet.create({});
